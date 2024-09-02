453494b1f8a503f79d7c68a4822f7d307b1ed482
Jacob Ebey <jacob.ebey@live.com>
2024-09-02 12:17:49 -0700

    fixup windows

--- startup.cjs
+++ startup.cjs
@@ -30,9 +30,14 @@ app.disable("x-powered-by");
 
 app.use(
   "/assets",
-  express.static("build/client/assets", { immutable: true, maxAge: "1y" })
+  express.static(path.resolve(__dirname, "build/client/assets"), {
+    immutable: true,
+    maxAge: "1y",
+  })
 );
-app.use(express.static("build/client", { maxAge: "1h" }));
+app.use(
+  express.static(path.resolve(__dirname, "build/client"), { maxAge: "1h" })
+);
 app.all("*", remixHandler);
 
 const port = Number.parseInt(process.env.PORT || "3000");
