c2385b2a920f23ba3ff41a3b56a40c7f8f261729
Jacob Ebey <jacob.ebey@live.com>
2024-09-02 02:05:33 -0700

    inline server instead of relying on serve package

--- startup.cjs
+++ startup.cjs
@@ -3,10 +3,11 @@
 const os = require("node:os");
 const path = require("node:path");
 
+const { createRequestHandler } = require("@remix-run/express");
 const Database = require("better-sqlite3");
-const { exec } = require("child_process");
 const { drizzle } = require("drizzle-orm/better-sqlite3");
 const { migrate } = require("drizzle-orm/better-sqlite3/migrator");
+const express = require("express");
 
 const sqlite = new Database(
   path.join(os.homedir(), ".localllama", "db.sqlite")
@@ -17,8 +18,21 @@ migrate(db, {
   migrationsFolder: path.join(__dirname, "migrations"),
 });
 
-const child = exec("node_modules/.bin/remix-serve build/server/index.js");
+const remixHandler = createRequestHandler({
+  build: () => import("./build/server/index.js"),
+});
 
-child.stdout.pipe(process.stdout);
-child.stderr.pipe(process.stderr);
-process.stdin.pipe(child.stdin);
+const app = express();
+app.disable("x-powered-by");
+
+app.use(
+  "/assets",
+  express.static("build/client/assets", { immutable: true, maxAge: "1y" })
+);
+app.use(express.static("build/client", { maxAge: "1h" }));
+app.all("*", remixHandler);
+
+const port = Number.parseInt(process.env.PORT || "3000");
+app.listen(port, () =>
+  console.log(`Local Llama server listening at http://localhost:${port}`)
+);
